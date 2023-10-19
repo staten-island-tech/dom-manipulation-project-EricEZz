ddc19c6409bc11d03c8765d44c058482d925a027
EricEZz <zhangeric343@gmail.com>
2023-10-19 10:15:46 -0400

    ad

--- dom.js
+++ dom.js
@@ -2,7 +2,7 @@ const DOMSelector = {
     form:document.querySelector("#form"),
     firstName:document.querySelector(".first-name"),
     h2s:document.querySelectorAll("h2"),
-}
+};
 DOMSelectors.form.addEventListener("submit",function(event){
     event.preventDefault();
     console.log(DOMSelectors.firstName.value);
@@ -12,3 +12,9 @@ DOMSelectors.form.addEventListener("submit",function(event){
      (el)=> (el.textContent = DOMSelectors.firstName.value)
     );
  });
+
+ const DOMSelectors = {
+    button: document.getElementById("btn"),
+    box: document.getElementById("container-box"),
+    input: document.querySelector('#input'),
+};
