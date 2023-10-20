21bce424b5188d945f20f69488722118d2c6dace
EricEZz <zhangeric343@gmail.com>
2023-10-20 10:15:48 -0400

    a

--- dom.js
+++ dom.js
@@ -1,20 +1,20 @@
-const DOMSelector = {
+const DOMSelectors = {
     form:document.querySelector("#form"),
-    firstName:document.querySelector(".first-name"),
+    Name:document.querySelector(".name"),
     h2s:document.querySelectorAll("h2"),
-};
-DOMSelectors.form.addEventListener("submit",function(event){
-    event.preventDefault();
-    console.log(DOMSelectors.firstName.value);
-    userobject
-    cardobject 
-    DOMSelectors.h2s.forEach(
-     (el)=> (el.textContent = DOMSelectors.firstName.value)
-    );
- });
-
- const DOMSelectors = {
     button: document.getElementById("btn"),
     box: document.getElementById("container-box"),
     input: document.querySelector('#input'),
 };
+DOMSelectors.form.addEventListener("submit",function(event){
+    event.preventDefault();
+    console.log(DOMSelectors.name.value);
+    
+ });
+
+ /* DOMSelectors.form.addEventListener("click",function(){
+    preventDefault();
+    let input = DOMSelector.input.value;
+    console.log(input);
+ });
+*/
