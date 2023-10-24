a64544ba9fb19215e03ec92038f7e9686314f52d
EricEZz <zhangeric343@gmail.com>
2023-10-24 10:13:44 -0400

    d

--- dom.js
+++ dom.js
@@ -1,20 +1,14 @@
 const DOMSelectors = {
     form:document.querySelector("#form"),
-    Name:document.querySelector(".name"),
+    Name:document.querySelector(".firstname"),
     h2s:document.querySelectorAll("h2"),
     button: document.getElementById("btn"),
     box: document.getElementById("container-box"),
     input: document.querySelector('#input'),
 };
+console.log(DOMSelectors.h2s);
 DOMSelectors.form.addEventListener("submit",function(event){
     event.preventDefault();
-    console.log(DOMSelectors.name.value);
+    console.log(DOMSelectors.firstname.value);
     
  });
-
- /* DOMSelectors.form.addEventListener("click",function(){
-    preventDefault();
-    let input = DOMSelector.input.value;
-    console.log(input);
- });
-*/
