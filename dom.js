556fe0d99bc0eb91dbd1afb41e1ab91bdfca1478
EricEZz <zhangeric343@gmail.com>
2023-10-27 10:13:21 -0400

    da

--- dom.js
+++ dom.js
@@ -1,13 +1,20 @@
 const DOMSelectors = {
-   container: document.querySelector(".top"),
+   container: document.querySelector(".box"),
   button: document.querySelector("#button"),
   topinput: document.querySelector("#Top-text"),
   bottominput: document.querySelector("#bottom-text"),
+  submit: document.querySelector()
   //image: document.image.querySelector("#image")
 };
 
+function addCard(){
+    document.querySelector('#button')
+
+}
+
+DOMSelectors
+
 DOMSelectors.button.addEventListener("click", function(){
-    
     const texttopinput = 
     DOMSelectors.topinput.value; 
     console.log(texttopinput);
@@ -42,3 +49,16 @@ function object(){}
  let input = DOMSelectors.value;
  DOMSelectors.Name.insertAdjacentHTML("afterend", '<p> ${input} </p>');
  */
+
+ //<div class="gallery">
+ /*<div class="card">
+ <h2 class=card></h2>
+     </div>
+ </div>
+ <form action="" id= "form">
+     <label for="firstname">Name</label>
+     <input type="text" name="firstname" class="first-name">
+     <input type="submit" value="Enter Name" id="">
+     <h2></h2>
+</form>
+</input> */ 
